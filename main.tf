bc55206f6d042c51bf2cf55fd50085c81eb10214
gahee.jung <gahee.jung@samsung.com>
2026-07-21 15:46:10 +0900

    main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  backend "local" {
-    path = "/states/dev/dev-gcp.tfstate"
-  }
-
-  required_version = ">= 1.8.5"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.21.0"
-    }
-  }
-}
-
 provider "google" {
   project = "temp-gcp-project-id-for-test"
 }
